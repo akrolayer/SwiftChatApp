056122a3ed03ce9c87d64b01fb91f538bf8e0bc7
akrolayer <almagestf532@gmail.com>
2020-05-28 23:58:51 +0900

    ChatApp UI WIP

---
+++ chatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by akrolayer on 2020/05/26.
+//  Copyright © 2020 akrolayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
