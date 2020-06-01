92a06cc863f81db549e6c235efcbb076be46c1ce
akrolayer <almagestf532@gmail.com>
2020-06-02 00:05:06 +0900

    register

--- RegisterViewController.swift
+++ RegisterViewController.swift
@@ -6,16 +6,43 @@
 //
 
 import UIKit
+import Firebase
+import Lottie
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    let animationView = AnimationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    //Firebaseにユーザを作成する
+    @IBAction func registerNewUser(_ sender: Any) {
+        //アニメーションのスタート
+        
+        //新規登録
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil{
+                print(error as Any)
+            }else{
+                print("ユーザの作成に成功しました！")
+            }
+        }
+    }
+    func startAnimation(){
+        let animation = Animation.named("loading")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height/1.5)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+    }
     /*
     // MARK: - Navigation
 
